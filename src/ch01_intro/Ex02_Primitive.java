fa9f436cc633c890b68bacd6fca8e8b37eab4e14
YooHeej <sprite1139@gmail.com>
2024-01-15 12:52:03 +0900

    first commit

---
+++ src/ch01_intro/Ex02_Primitive.java
@@ -0,0 +1,19 @@
+package ch01_intro;
+
+public class Ex02_Primitive {
+
+	public static void main(String[] args) {
+		// boolean
+		boolean a = true, b = false;
+		
+		/*
+		 *  정수 계열
+		 *  	1. byte : 1 바이트, 범위 : -128 ~ 127
+		 *  	2. short: 2 바이트, 범위 : -32768 ~ 32767
+		 *  	3. int:   4 바이트, 범위 : -2147483648 ~  2147483647
+		 *  	4. long:  8 바이트, 범위 : -2**63 ~ 2** 63 - 1
+		 */
+
+	}
+
+}
