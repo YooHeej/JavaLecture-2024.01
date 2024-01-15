c7839010601f7e850de49a85343c35568a73b762
YooHeej <sprite1139@gmail.com>
2024-01-15 15:00:14 +0900

    2장 자료형

---
+++ src/ch02_type/Ex02_Primitive.java
@@ -0,0 +1,45 @@
+package ch01_intro;
+
+public class Ex02_Primitive {
+
+	public static void main(String[] args) {
+		// boolean
+		boolean a = true, b = false;
+		System.out.println("a: " + a + ", b:" + b);
+		
+		/*
+		 *  정수 계열
+		 *  	1. byte : 1 바이트, 범위 : -128 ~ 127
+		 *  	2. short: 2 바이트, 범위 : -32768 ~ 32767
+		 *  	3. int:   4 바이트, 범위 : -2147483648 ~  2147483647
+		 *  	4. long:  8 바이트, 범위 : -2**63 ~ 2** 63 - 1
+		 *  
+		 *  주의 사항
+		 *  	- 숫자값(literal)(예: -123, 57)은 기본적으로 int 타입
+		 *  	- long 타입의 숫자값 뒤에는 L을 붙여야 함(15L)
+		 */
+		byte w = 10;
+		short x = 1024;
+		int y = 1000000;
+		long z = 300000000L;
+		System.out.printf("%d, %d, %d, %d\n", w, x, y, z);		// format, d-정수, f-실수, 
+		
+		/*
+		 * 실수 계열
+		 * 		1. float : 4바이트, 범위(유효자리) : 소수점 이하 7자리
+		 * 		2. double: 8바이트, 범위(유효자리) : 소수점 이하 15자리
+		 * 주의 사항
+		 * 		- 숫자값(예: 3.14, 2.728)은 기본적으로 double 타입
+		 * 		- float  타입의 숫자값 뒤에는 F를 붙여야 함(3.14F)
+		 */
+		float f1 = 1.0000001F, f2 = 1.00000001F;
+		double d1 = 1.00000000001, d2 = 1.000000000000001;
+		System.out.println("f1: " + f1 + ", f2: " +f2);
+		System.out.println("d1: " + d1 + ", d2: " + d2);
+		
+		// 문자 : 1글자
+		char eng = 'A', kor = '안';
+		System.out.println("eng: " + eng + ", kor: " + kor);
+	}
+
+}
