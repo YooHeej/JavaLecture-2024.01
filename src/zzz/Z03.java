f2633e4279432ba688ecf74e3a3be790f9037fd3
YooHeej <sprite1139@gmail.com>
2024-01-17 02:36:08 +0900

    Update Z03.java

--- src/zzz/Z03.java
+++ src/zzz/Z03.java
@@ -5,8 +5,12 @@ public class Z03 {
 	public static void main(String[] args) {
 //0, 2, 4, 6, 8, 10		
 		for(int i = 0; ; i++ ) {
-			if()
-			
+			if(i % 2 == 1) {
+				continue;
+			}
+			if(i >10) {
+				break;
+			}
 			System.out.println(i);
 		}
 	}
